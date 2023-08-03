e3571ca78b6b15811e0bf650de67f4b114ccdd7f
Andrey <a.egerev@gmail.com>
2023-08-03 10:56:21 +0300

    add home work seminar 6 Python

--- HW_python_seminar6/Task_32/main.py
+++ HW_python_seminar6/Task_32/main.py
@@ -4,24 +4,25 @@
 # максимума)
 
 def CreateArray(n):
-    numbers1 = []
+    numbers = []
     for i in range(n):
-        numbers1.append(int(input(f"Введите {i+1}-й элемент: ")))
-    return numbers1
+        numbers.append(int(input(f"Введите {i+1}-й элемент: ")))
+    return numbers
 
 def SortArray (numbers, rangeNum):
     indexNumbers =[]
-    for i in range(len(numbers1)-1):
-        if numbers1[i] >= rangeNum[0] and numbers1[i] <= rangeNum[1]:
-            indexNumbers.append(numbers1[i])
+    for i in range(len(numbers)):
+        if numbers[i] >= min(rangeNum) and numbers[i] <= max(rangeNum):
+            indexNumbers.append(i)
     return indexNumbers
 
-
 n = int(input("Введите кол-во элементов первого множества: "))
-
 numbers1 = CreateArray(n)
 print(numbers1)
+
 print("Введите диапазон отбора: ")
 rangeNumber = CreateArray(2)
 print(rangeNumber)
+
+print("Индексы элементов входящих в заданый диапазон: ")
 print(SortArray(numbers1, rangeNumber))
